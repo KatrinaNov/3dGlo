30d62f7c3caa7722534a8aaa4a13635560c0e9da
katrina <katrina.nov13@gmail.com>
2020-06-29 12:02:55 +0300

    timer

--- script/script.js
+++ script/script.js
@@ -4,30 +4,41 @@ window.addEventListener('DOMContentLoaded', function() {
   function countTimer(deadline) {
     let timerhours = document.querySelector('#timer-hours'),
         timerminutes = document.querySelector('#timer-minutes'),
-        timerseconds = document.querySelector('#timer-seconds');
+        timerseconds = document.querySelector('#timer-seconds'),
+        timeInterval = setInterval(updateClock, 1000);
 
-        function getTimeRemaining() {
-          let dateStop = new Date(deadline).getTime(),
-          dateNow = new Date().getTime(),
-          timeRemaining = (dateStop - dateNow) / 1000,
-          seconds = Math.floor(timeRemaining % 60),
-          minutes = Math.floor((timeRemaining / 60) % 60),
-          hours = Math.floor((timeRemaining / 3600) % 24);
-          // day = Math.floor(timeRemaining / 3600 /24)
-          return {timeRemaining, hours, minutes, seconds};
-        }
-        function updateClock() {
-          let timer = getTimeRemaining();
+    function getTimeRemaining() {
+      let dateStop = new Date(deadline).getTime(),
+      dateNow = new Date().getTime(),
+      timeRemaining = (dateStop - dateNow) / 1000,
+      seconds = Math.floor(timeRemaining % 60),
+      minutes = Math.floor((timeRemaining / 60) % 60),
+      hours = Math.floor((timeRemaining / 3600));
+      // day = Math.floor(timeRemaining / 3600 /24)
+      return {timeRemaining, hours, minutes, seconds};
+    }
 
-          timerhours.textContent = timer.hours;
-          timerminutes.textContent = timer.minutes;
-          timerseconds.textContent = timer.seconds;
-          if (timer.timeRemaining > 0) {
-            setTimeout(updateClock, 1000);
-          }
-        }
-        updateClock();
-        
+    function addZero(num) {
+      if (num <= 9) {
+        return '0' + num;
+      } else {
+        return num;
+      }
+    }
+    function updateClock() {
+      let timer = getTimeRemaining();
+      timerhours.textContent = addZero(timer.hours);
+      timerminutes.textContent = addZero(timer.minutes);
+      timerseconds.textContent = addZero(timer.seconds);
+      
+      // когда время вышло, останавливаем отсчет и обнуляем время
+      if (timer.timeRemaining <= 0) {
+        clearInterval(timeInterval);
+        timerhours.textContent = '00';
+        timerminutes.textContent = '00';
+        timerseconds.textContent = '00';
+      }
+    }        
   }
-  countTimer('01 July 2020');
+  countTimer('2 july 2020');
 });
