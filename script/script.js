2160b7013b3aea4e1b23c640a605e83d9381df34
katrina <katrina.nov13@gmail.com>
2020-07-06 11:58:39 +0300

    изменение фото при наведении

--- script/script.js
+++ script/script.js
@@ -257,4 +257,20 @@ window.addEventListener('DOMContentLoaded', () => {
     startSlide(2000);
   };
   slider();
+
+  // наша команда
+  const changePhoto = () => {
+    const commandPhoto = document.querySelectorAll('.command__photo');
+    commandPhoto.forEach(item => {
+      const src = item.src;
+      item.addEventListener('mouseenter', e => {
+        e.target.src = e.target.dataset.img;
+      });
+      item.addEventListener('mouseout', e => {
+        e.target.src = src;
+      });
+
+    });
+  };
+  changePhoto();
 });
