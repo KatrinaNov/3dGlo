8534b6d91ea46f84e19f20913c7eb003fc1a9f04
katrina <katrina.nov13@gmail.com>
2020-07-07 12:28:26 +0300

    calculator

--- script/script.js
+++ script/script.js
@@ -275,13 +275,49 @@ window.addEventListener('DOMContentLoaded', () => {
   changePhoto();
 
   // калькулятор
-  const checkNumber = () => {
+  const calc = (price = 100) => {
     const calcInput = document.querySelectorAll('.calc-item[type=text]');
     calcInput.forEach(item => {
       item.addEventListener('input', () => {
         item.value = item.value.replace(/\D/g, '');
       });
     });
+
+    const calcBlock = document.querySelector('.calc-block'),
+      calcType = document.querySelector('.calc-type'),
+      calcCount = document.querySelector('.calc-count'),
+      calcSquare = document.querySelector('.calc-square'),
+      calcDay = document.querySelector('.calc-day'),
+      totalValue = document.getElementById('total');
+
+    const countSum = () => {
+      let total = 0,
+        countValue = 1,
+        dayValue = 1;
+      const typeValue = calcType.options[calcType.selectedIndex].value,
+        squareValue = +calcSquare.value;
+      if (calcCount.value > 1) {
+        countValue += (calcCount.value - 1) / 10;
+      }
+      if (calcDay.value && calcDay.value < 5) {
+        dayValue *= 2;
+      } else if (calcDay.value && calcDay.value < 10) {
+        dayValue *= 1.5;
+      }
+
+      if (typeValue && squareValue) {
+        total = price * typeValue * squareValue * countValue * dayValue;
+      }
+      totalValue.textContent = total;
+    };
+    calcBlock.addEventListener('input', event => {
+      const target = event.target;
+
+      if (target.matches('select') || target.matches('input')) {
+        countSum();
+      }
+    });
+
   };
-  checkNumber();
+  calc(100);
 });
